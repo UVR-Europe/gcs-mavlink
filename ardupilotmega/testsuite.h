c7eb533793a09cbbee33b912e5e5a5cbdfe0ba47
Aleksei Makurin <mav@uvr.aero>
2022-05-31 13:32:31 +0200

    UVR_GENERIC_DATA added

--- ardupilotmega/testsuite.h
+++ ardupilotmega/testsuite.h
@@ -4171,6 +4171,61 @@ static void mavlink_test_uvr_pilot_id(uint8_t system_id, uint8_t component_id, m
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_uvr_generic_data(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_UVR_GENERIC_DATA >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_uvr_generic_data_t packet_in = {
+        "ABC",{ 17, 18, 19, 20 }
+    };
+    mavlink_uvr_generic_data_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        
+        mav_array_memcpy(packet1.id, packet_in.id, sizeof(char)*4);
+        mav_array_memcpy(packet1.data, packet_in.data, sizeof(uint8_t)*4);
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_UVR_GENERIC_DATA_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_UVR_GENERIC_DATA_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_uvr_generic_data_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_uvr_generic_data_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_uvr_generic_data_pack(system_id, component_id, &msg , packet1.id , packet1.data );
+    mavlink_msg_uvr_generic_data_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_uvr_generic_data_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.id , packet1.data );
+    mavlink_msg_uvr_generic_data_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_uvr_generic_data_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_uvr_generic_data_send(MAVLINK_COMM_1 , packet1.id , packet1.data );
+    mavlink_msg_uvr_generic_data_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_sensor_offsets(system_id, component_id, last_msg);
@@ -4243,6 +4298,7 @@ static void mavlink_test_ardupilotmega(uint8_t system_id, uint8_t component_id, 
     mavlink_test_uvr_pid_gov(system_id, component_id, last_msg);
     mavlink_test_uvr_can_status(system_id, component_id, last_msg);
     mavlink_test_uvr_pilot_id(system_id, component_id, last_msg);
+    mavlink_test_uvr_generic_data(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
